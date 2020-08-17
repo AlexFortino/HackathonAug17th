e3f82cdc2a4a04f35cda692874e6ae732af93844
Readus1 <69687210+Readus1@users.noreply.github.com>
2020-08-17 10:45:41 -0700

    Added Small Brian Enemy

---
+++ TeamGame/Assets/Scripts/EnemySmallBrain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+
+public class EnemySmallBrain : MonoBehaviour
+{
+
+    GameObject Player;
+    NavMeshAgent EAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        EAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        EAgent.SetDestination(Player.transform.position);
+    }
+}
