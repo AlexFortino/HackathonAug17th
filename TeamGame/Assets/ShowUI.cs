3ee0b181ae549f5fbac6edb0f877ce074d0814ae
AndreiPiterbarg <andrei.piterbarg@yahoo.com>
2020-08-17 18:01:24 +0200

    Started working on tutorial

---
+++ TeamGame/Assets/ShowUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject uiObject;
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            uiObject.SetActive(true);
+            StartCoroutine("WaitForSec");
+        }
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(uiObject);
+        Destroy(gameObject);
+    }
+
+
+
+
+
+}
