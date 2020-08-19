124503a9e437f336a1617f10991e5fc5b1b693be
AndreiPiterbarg <andrei.piterbarg@yahoo.com>
2020-08-19 16:26:09 +0200

    Tile Set and Menu

---
+++ TeamGame/Assets/Scripts/MenuControler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControler : MonoBehaviour
+{
+
+    public GameObject pauseMenu;
+    public bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(isPaused)
+            {
+                ResumeGame();
+            }
+
+            else
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.Confined;
+                isPaused = true;
+                pauseMenu.SetActive(true);
+                Time.timeScale = 0f;
+
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void ReturnToMain()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
