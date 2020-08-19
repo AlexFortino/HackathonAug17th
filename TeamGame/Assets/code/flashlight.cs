a88d480e19081fe771af921f02535df63d028c45
KrabsDEV <krabssan@gmail.com>
2020-08-19 10:06:12 -0400

    player master fps
    good fps better than sliced bread includes a gucci flashlight with a worth of 10,000,00

---
+++ TeamGame/Assets/code/flashlight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashlight : MonoBehaviour
+{
+    private Light mylight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mylight = GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+
+            mylight.enabled = !mylight.enabled;
+        }
+    }
+}
