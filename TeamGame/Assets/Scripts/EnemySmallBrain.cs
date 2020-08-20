a5398036b8388113ef01b89738e4b754729f980f
Readus1 <69687210+Readus1@users.noreply.github.com>
2020-08-20 10:47:09 -0700

    BIG brain Small brian

--- TeamGame/Assets/Scripts/EnemySmallBrain.cs
+++ TeamGame/Assets/Scripts/EnemySmallBrain.cs
@@ -12,6 +12,8 @@ public class EnemySmallBrain : MonoBehaviour
     NavMeshAgent EAgent;
     bool GlowstickExists = false;
     bool ChasePlayer = true;
+    bool Cooldown = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -24,35 +26,57 @@ public class EnemySmallBrain : MonoBehaviour
     void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.Q)) {
+    //   // if (Input.GetKeyDown(KeyCode.Q)) {
 
-            Glowstick.SetActive(true);
+            //        Glowstick.SetActive(true);
 
+
+            //    }
+
+            //    if (Glowstick.activeSelf && GlowstickExists == false)
+            //    {
+            //        EAgent.SetDestination(Glowstick.transform.position);
+            //        GlowstickExists = true;
+            //        ChasePlayer = false;
+            //        Invoke("glowlife", 10f);
+            //    }
+
+        //else if (ChasePlayer == true)
+        //{
+           //EAgent.SetDestination(Player.transform.position);
+        //}
+
+
+        if (Cooldown == false) { EAgent.SetDestination(Player.transform.position);}
 
         }
 
-        if (Glowstick.activeSelf && GlowstickExists == false)
-        {
-            EAgent.SetDestination(Glowstick.transform.position);
-            GlowstickExists = true;
-            ChasePlayer = false;
-            Invoke("glowlife", 10f);
-        }
-
-        else if (ChasePlayer == true)
-        {
-            EAgent.SetDestination(Player.transform.position);
-        }
-
-
-
-    }
-
-    void glowlife() {
+        void glowlife() {
 
         ChasePlayer = true;
 
     }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")&& Cooldown == false)
+        {
+            Cooldown = true;
+            Player.GetComponent<PlayerMovement>().playerHP-=1;
+            Invoke("AttackCooldown",  1.5f);
+            EAgent.isStopped = true;
+        }
+    }
+
+    void AttackCooldown()
+    {
+
+        Cooldown = false;
+        EAgent.isStopped = false;
+    }
+
+
+    
 
 }
 
