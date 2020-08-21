37840e68565f72f80197ac80a0c46fd1b2a213ce
AndreiPiterbarg <andrei.piterbarg@yahoo.com>
2020-08-21 10:37:53 +0200

    A lot

---
+++ TeamGame/Assets/TeleportingDoor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleportingDoor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+
+        }
+    }
+
+}
